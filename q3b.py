04157631e9748ef41d21abba694ffaf148453287
Donald Whyte <donaldwhyte0@gmail.com>
2013-11-15 16:44:20 +0000

    Implemented binary mutual information cmputer

--- q3b.py
+++ q3b.py
@@ -1,3 +1,4 @@
+import sys
 import random
 import time
 import math	
@@ -5,72 +6,59 @@ import nltk
 from nltk.corpus import movie_reviews, stopwords
 from cw1_base import *
 
-class MutualInformationAssociator:
+class BinaryMutualInformationAssociator:
 
-	def __init__(self, allWords, allClasses, documents): # documents = list of tuples (words, cls)
+	def __init__(self, classToAssociate, allWords, documents):
 		# n11 = number of docs with current class that have current word
 		# n10 = number of docs with current word that are NOT in the current class
 		# n01 = number of docs without current word, but are the current class
 		# n00 = number of docs that neither contain the current word nor are the current class
 
-		# Create lists of word mutual information values for each class
-		self.association = {}
-		for cls in allClasses:
-			self.association[cls] = {}
-		# Look through all classes to maintain dictionaries
-		associationMatrices = {}
-		for cls in allClasses:
-			associationMatrices[cls] = {}
-			for word in allWords:
-				associationMatrices[cls][word] = [ [0, 0], [0, 0] ]
-		# Compute class frequencies
-		clsFrequencies = {} # TODO
-		# Loop through all the labelled documents
+		self.classToAssociate = classToAssociate
+
+		# Construct dictionary which maps words to their associate with CLASS ONE
+		self.association = { }
+		# Construct empty association matrices for each (relative to CLASS ONE!)
+		associationMatrices = { }
+		for word in allWords:
+			associationMatrices[word] = [ [0, 0], [0, 0] ]
+		# Count values of matrices incrementally
 		for docWords, docClass in documents:
-			docWords = set(docWords) # convert to set for O(1) membership test
+			# Convert to set for O(1) membership test
+			docWords = set(docWords)
+			# Check if document's class is the one being used for association
+			isAssociationClass = (docClass == self.classToAssociate)
+			# Now check all words in document and see if they occur in 
 			for word in allWords:
 				if word in docWords:
-					associationMatrices[docClass][word][1][1] += 1
+					if isAssociationClass:
+						associationMatrices[word][1][1] += 1
+					else:
+						associationMatrices[word][1][0] += 1		
 				else:
-					associationMatrices[docClass][word][0][1] += 1
-			# TODO: use class frequencues
-			associationMatrices[docClass][word][1][0] = (total - associationMatrices[docClass][word][1][1])
-			associationMatrices[docClass][word][0][0] = (total - associationMatrices[docClass][word][0][1])
+					if isAssociationClass:
+						associationMatrices[word][0][1] += 1
+					else:
+						associationMatrices[word][0][0] += 1
+		# # Compute mutual information from these matrices
+		for word in allWords:
+			self.association[word] = self.computeMutualInfo( associationMatrices[word] )
 
-		# TODO: 
+	def computeMutualInfo(self, mat):
+		"""Return mutual information for class/word given frequency matrix represents."""
+		total = mat[1][1] + mat[1][0] + mat[0][1] + mat[0][0]	
+		return (mat[1][1] / total) * self.applyLog( (total * mat[1][1]) / max(1, ((mat[1][1] + mat[1][0]) * (mat[1][1] + mat[0][1]))) ) + \
+			(mat[0][1] / total) * self.applyLog( (total * mat[0][1]) / max(1, ((mat[0][1] + mat[0][0]) * (mat[1][1] + mat[0][1]))) ) + \
+			(mat[1][0] / total) * self.applyLog( (total * mat[1][0]) / max(1, ((mat[1][1] + mat[1][0]) * (mat[1][0] + mat[0][0]))) ) + \
+			(mat[0][0] / total) * self.applyLog( (total * mat[0][0]) / max(1, ((mat[0][1] + mat[0][0]) * (mat[1][0] + mat[0][0]))) ) 
 
-		# # Compute mutual information from these counts
-		# total = n11 + n10 + n01 + n00
-		# divisor = max(1, ((n11 + n10) * (n11 + n01)))
-		# logOperand = ((total * n11) / divisor)
-		# logOperand = max(0.000000000001, logOperand)
-		# mutInfo = (n11 / total) + math.log(logOperand)
-		# self.association[cls].append(mutInfo)
+	def applyLog(self, value):
+		return math.log( max(0.000000000001, logOperand) )
 
-	def associate(self, word, cls):
-		return self.association[cls].get(word, 0.0)
-
-class FeatureSelector:
-
-	def __init__(self, associator, allClasses):
-		self.associator = associator
-		self.allClasses = allClasses
-
-	def select(self, words, k):
-		perClass = k / len(self.allClasses)
-		leftover = k % len(self.allClasses)
-
-		selectedWords = []
-		for cls in self.allClasses:
-			rankedWords = sorted(words, key = lambda x : self.associator.associate(x, cls))
-			if leftover > 0: # also add an extra feature due to upper bound
-				upperBound = perClass + 1
-				leftover -= 1
-			else:
-				upperBound = 0
-			selectedWords += rankedWords[:upperBound]
-		return selectedWords
-
+	def select(self, words, numToSelect):
+		# Rank words based on their association with chosen class
+		rankedWords = sorted(words, key = lambda x : self.association[word], reverse=True)	
+		return rankedWords[:numToSelect]
 
 if __name__ == "__main__":
 	# Parse command line arguments
@@ -89,10 +77,9 @@ if __name__ == "__main__":
 	# Determine which features to keep
 	print "DECIDING FEATURES"
 	# Build associator to rank words and classes
-	associator = MutualInformationAssociator(allWords, allClasses, dataset)
+	associator = BinaryMutualInformationAssociator("pos", allWords, dataset)
 	# Select specified number of features and construct feature extractor
-	selector = FeatureSelector(associator, allClasses)
-	selectedWords = selector.select(allWords, NUM_FEATURES)
+	selectedWords = associator.select(allWords, NUM_FEATURES)
 	featureExtractor = BinomialExtractor(selectedWords)
 	# Build and train a classifier which uses a DIFFERENT feature extractor
 	print "TRAINING CLASSIFIER"
